e6a1fd8158fc4ddaf76ac23f10428d11fc5905cd
Sriram Pohar Maddirala <ram@srirams-mbp.myfiosgateway.com>
2023-06-13 01:13:56 -0400

    synced post id url

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,9 +1,9 @@
-export default function page({params}) {
-  if(params){
-    console.log(params.id)
-  }
+export default function page({params, id}) {
+    if(params){
+      id = params.id
+    }
     return (
-      <div className="flex p-4 border-b">
+      <div className="flex p-4">
         <div className="flex-shrink-0">
           <img
             className="h-12 w-12 rounded-full"
@@ -13,7 +13,7 @@ export default function page({params}) {
         </div>
         <div className="ml-4">
           <h3 className="text-xl font-semibold text-gray-600">John Doe</h3>
-          <p className="text-gray-600">Commencing the forum with post</p>
+          <p className="text-gray-600">Commencing the forum with post {id}</p>
           <div className="flex items-center mt-2"></div>
         </div>
       </div>
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,12 +1,18 @@
 import Post from '@/app/post/[id]/page'
+import Link from 'next/link'
+
 export default function page() {
   return (
     <div className="container mx-auto py-8">
       <div className="max-w-2xl mx-auto bg-white rounded-lg shadow">
         <div className="p-4 border-b">
-          <h2 className="text-lg font-semibold">Feed</h2>
-        </div>          
-          <Post/>
+          <h2 className="text-lg text-gray-600 font-semibold">Feed</h2>
+        </div>  
+        <Link
+            href="/post/1"
+            className="text-gray-600 hover:bg-blue-200 rounded-full p-3 mb-4">
+              <Post id={1}/>
+            </Link>        
       </div>
     </div>
   );
